fb9d2dfa49c20573876a138f06e70eaa2789cb5d
Aditya Narayan <narayanaditya95@gmail.com>
2015-03-17 04:18:08 +0530

    ALPHA:Working Local Deploy

--- tardis_simulator/views.py
+++ tardis_simulator/views.py
@@ -4,18 +4,22 @@ from graphos.renderers import gchart
 from graphos.sources.simple import SimpleDataSource
 from tardis import run_tardis
 
-
 def home(request):
 	return render(request, 'tardis_simulator/home.html')
 
 def default_chart(request):
-	g_chart = gchart.LineChart(SimpleDataSource(data=run_default()))		
+	g_chart = gchart.LineChart(SimpleDataSource(data=run_default()))
 	return render(request,'tardis_simulator/demo.html',{
-			'chart': gchart
+			'chart': g_chart,
 		})
 
 def run_default():
 	model = run_tardis(
-		'tardis_simulator/static/tardis_example/tardis_example.yml')
-	return zip(mdl.spectrum.luminosity_density_lambda.value,
-		mdl.spectrum.wavelength.value)
+		'tardis_simulator/static/tardis_example/tardis_example.yml',
+		'tardis_simulator/static/tardis_example/kurucz_cd23_chianti_H_He.h5')
+	print model.spectrum.luminosity_density_lambda.value
+	# print model.spectrum.wavelength.value
+	data =  zip(model.spectrum.wavelength.value,
+		model.spectrum.luminosity_density_lambda.value)
+	data.insert(0, ('Wavelength', 'Luminosity Density'))
+	return data
